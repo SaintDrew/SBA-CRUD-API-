a87004eb460822d3dfdc03ea2ac598620e622df6
SaintDrew <drew_el12@yahoo.com>
2024-06-06 23:18:50 -0400

    Early Stages

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose')
+
+const app = express() 
+
+
+
+app.get('/', (req, res) => {
+    res.send("Hello from Node API Server Updated");
+});
+
+mongoose.connect("mongodb+srv://SaintAdmin:AbJbxATNvsbmE6Iz@clusterdb1.wwdokmu.mongodb.net/Node-API?retryWrites=true&w=majority&appName=ClusterDB1")
+.then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () =>{
+        console.log('Server is running on port 3000');
+    });
+})
+.catch(() => {
+console.log("Connection Failed!");
+});
