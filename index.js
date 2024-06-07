02196567e834c5749466af8373ba3bab3cdb1839
SaintDrew <drew_el12@yahoo.com>
2024-06-06 23:37:14 -0400

    building models

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 mongoose.connect("mongodb+srv://SaintAdmin:AbJbxATNvsbmE6Iz@clusterdb1.wwdokmu.mongodb.net/Node-API?retryWrites=true&w=majority&appName=ClusterDB1")
 .then(() => {
     console.log("Connected to database!");
-    app.listen(3000, () =>{
+    app.listen(3000, () => {
         console.log('Server is running on port 3000');
     });
 })
