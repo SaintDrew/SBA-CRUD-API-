3ca8f86b6db8792f63b7c945eb13787e5f643016
SaintDrew <drew_el12@yahoo.com>
2024-06-14 09:33:10 -0400

    Updated Routes and controllers

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const postRoute = require("./server/routes/post.route");
+const postRoute = require("./routes/routes/post.route.js");
 const Post = require('/Users/macbook/sei_classwork/319-mongodb/mongodb-SBA/models/post.model.js');
 const app = express();
 
@@ -18,68 +18,6 @@ app.get('/', (req, res) => {
     res.send("Hello from Node API Server");
 });
 
-
-app.get('/api/posts/', async (req, res) =>{
-    try{
-        const posts = await Post.find({});
-        res.status(200).json(posts);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-app.get('/api/posts/:id', async (req, res) =>{
-    try{
-        const { id } = req.params;
-        const post = await Post.findById(id);
-        res.status(200).json(post);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-app.post('/api/posts', async (req, res) =>{
-    try{
-        const post = await Post.create(req.body);
-        res.status(200).json(post)
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-// update a post
-app.put('/api/posts/:id', async (req, res) =>{
-    try{
-        const { id } = req.params;
-        const post = await Post.findByIdAndUpdate(id, req.body);
-
-        if (!post) {
-            return res.status(404).json({message: "Post not found"})
-        }
-
-        const updatedPost = await Post.findById(id);
-        res.status(200).json(updatedPost);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-// delete a Post 
-app.delete('/api/posts/:id', async (req, res)=> {
-    try{
-        const { id } = req.params;
-
-        const post = await Post.findByIdAndDelete(id);
-
-        if (!post) {
-            return res.status(404).json({message: "Post not found"});
-        }
-
-        res.status(200).json({message: "Post deleted successfully"});
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
 
 mongoose.connect("mongodb+srv://SaintAdmin:AbJbxATNvsbmE6Iz@clusterdb1.wwdokmu.mongodb.net/Node-API?retryWrites=true&w=majority&appName=ClusterDB1")
 .then(() => {
--- routes/routes/post.route.js
+++ routes/routes/post.route.js
@@ -2,19 +2,19 @@ const express = require("express");
 const Post = require('/Users/macbook/sei_classwork/319-mongodb/mongodb-SBA/models/post.model.js');
 const router = express.Router();
 const {getPosts} = require('mongodb-SBA/controllers/post.controllers.js')
-// const {getPosts, getPost, createPost, updatePost, deletePost} = require('../../controllers/post.controller');
+const {getPosts, getPost, createPost, updatePost, deletePost} = require('../../controllers/post.controller');
 
 
 router.get('/', getPosts);
 router.get("/:id", getPost);
 
-// router.post("/", createPost);
+router.post("/", createPost);
 
-// // updating a article
-// router.put("/:id", updatePost);
+// updating a article
+router.put("/:id", updatePost);
 
 // // deleting a article
 // router.delete("/:id", deletePost);
 
 
-// module.exports = router;
+module.exports = router;
